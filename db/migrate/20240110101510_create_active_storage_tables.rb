a170aa3a9129bc7bbe7855127357b8c0ad88644e
Koralle (Jitera) <developers@jitera.com>
2024-01-10 10:25:10 +0000

    fix: 1 issues from deployment report

--- db/migrate/20240110101510_create_active_storage_tables.rb
+++ db/migrate/20240110101510_create_active_storage_tables.rb
@@ -1,5 +1,6 @@
-# This migration comes from active_storage (originally 20170806125915)
-class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
+
+# This migration comes from active_storage (originally 20170806125915) - version 2
+class CreateActiveStorageTablesV2 < ActiveRecord::Migration[5.2]
   def change
     return if table_exists?(:active_storage_blobs)
 
