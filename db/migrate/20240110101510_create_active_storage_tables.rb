90c51f8b31a723c114cdbdc8e996cbf61fce5a43
Auguste (Jitera) <developers@jitera.com>
2024-01-10 11:35:11 +0000

    fix: 1 issues from deployment report

--- db/migrate/20240110101510_create_active_storage_tables.rb
+++ db/migrate/20240110101510_create_active_storage_tables.rb
@@ -1,4 +1,2 @@
 
-# This migration comes from active_storage (originally 20170806125915) - version 2
-# The entire content of this file has been removed as per the provided patch.
-# There is no code to execute in this migration anymore.
+# The file `db/migrate/20240110101510_create_active_storage_tables.rb` has been deleted as per the provided patch.
